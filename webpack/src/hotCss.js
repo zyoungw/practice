5719b3d7844f5e1671524b3ec0cc9a5a78e67d56
zyoungw <zyoungw@foxmail.com>
2020-07-30 17:09:31 +0800

    热模块更新

---
+++ webpack/src/hotCss.js
@@ -0,0 +1,10 @@
+import './index.css'
+console.log(888)
+var btn = document.createElement('button')
+btn.innerHTML = '新增'
+document.body.appendChild(btn)
+btn.onclick = function () {
+  var div = document.createElement('div')
+  div.innerHTML = 'item'
+  document.body.appendChild(div)
+}
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,61 +1,64 @@
- 
-const path = require("path"); 
-const htmlWebpackPlugin = require("html-webpack-plugin")
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
+
 module.exports = {
   // 必填 webpack执⾏行行构建⼊入⼝口 (字符串， 数组， 对象)
-  entry: "./src/index.js", 
+  entry: './src/hotCss.js',
   // entry: {
   //   main: "./src/index.js",
   //   other: "./src/other.js",
   //   test: "./src/test.js",
   // },
-  mode: "development",   // none development production
+  mode: 'development', // none development production
   // mode: "production",
   output: {
-    // 将所有依赖的模块合并输出到main.js 
+    // 将所有依赖的模块合并输出到main.js
     // filename: "main.js",
-    filename: "[name].js", // chunkHash hash contentHash 
+    filename: '[name].js', // chunkHash hash contentHash
     // 输出⽂文件的存放路路径，必须是绝对路路径
-    path: path.resolve(__dirname, "./dist")
+    path: path.resolve(__dirname, './dist'),
   },
-  devtool:"cheap-module-eval-source-map",
+  devtool: 'cheap-module-eval-source-map',
   module: {
     rules: [
       {
         test: /\.css$/,
         // 两个loader 有执行顺序 自左往右
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.png$/,
         // use: ["file-loader"]
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
             // name: "[name]_[hash].[ext]"
-            name: "[name].[ext]"
-          }
-        }
-      }
-    ]
+            name: '[name].[ext]',
+          },
+        },
+      },
+    ],
   },
   devServer: {
-    contentBase: path.resolve(__dirname,  "./dist"),
+    contentBase: path.resolve(__dirname, './dist'),
     open: true,
+    hot: true,
     port: 8082,
     proxy: {
-      "/api": {
-        target: "http://localhost:9092"
-      }
-    }
+      '/api': {
+        target: 'http://localhost:9092',
+      },
+    },
   },
   plugins: [
     new htmlWebpackPlugin({
-      title: "My App",
-      template: "./index.html",
-      filename: "index.html"
+      title: 'My App',
+      template: './index.html',
+      filename: 'index.html',
     }),
-    new CleanWebpackPlugin()
-  ]
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 }
