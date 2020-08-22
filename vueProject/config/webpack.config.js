27a7cfec518ef19306225f0f07b1784d2e21be77
zyoungw <zyoungw@foxmail.com>
2020-08-22 22:37:29 +0800

    first

---
+++ vueProject/config/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = {
+  // 必填 webpack执⾏构建⼊⼝
+  entry: "../src/index.js",
+  output: {
+  // 将所有依赖的模块合并输出到main.js
+filename: "../main.js",
+  // 输出⽂件的存放路径，必须是绝对路径
+  path: path.resolve(__dirname, "./dist")
+ }
+};
